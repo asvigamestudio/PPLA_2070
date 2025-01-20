2f1e06779c98bacbdc45625b9347ffcb691bcf3a
TraumaFalcon1314 <jamesdarmawan041@gmail.com>
2025-01-20 13:55:43 +0700

    Create Premium Feature.cpp

---
+++ Premium Feature.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Header file untuk input dan output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Menampilkan teks ke layar
+    return 0; // Mengembalikan nilai 0 untuk menunjukkan program berhasil
+}
